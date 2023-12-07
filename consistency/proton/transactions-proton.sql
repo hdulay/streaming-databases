96ef40b4447bea30da67b85bd54c14b45265f7e0
Jove Zhong <jove@timeplus.io>
2023-12-07 13:22:55 -0800

    use range JOIN to improve consistency

--- consistency/proton/transactions-proton.sql
+++ consistency/proton/transactions-proton.sql
@@ -6,11 +6,11 @@ CREATE EXTERNAL STREAM transactions(
     ts datetime64) 
 SETTINGS type='kafka', brokers='broker:29092', topic='transactions', data_format='JSONEachRow';
 
-CREATE VIEW credits AS SELECT to_account as account, sum(amount) as credits FROM transactions GROUP BY to_account;
+CREATE VIEW credits AS SELECT now64() as ts, to_account as account, sum(amount) as credits FROM transactions GROUP BY to_account EMIT PERIODIC 1s;
 
-CREATE VIEW debits AS SELECT from_account as account, sum(amount) as debits FROM transactions GROUP BY from_account;
+CREATE VIEW debits AS SELECT now64() as ts, from_account as account, sum(amount) as debits FROM transactions GROUP BY from_account EMIT PERIODIC 1s;
 
-CREATE VIEW balance AS SELECT credits.account, credits - debits as balance FROM credits, debits WHERE credits.account = debits.account;
+CREATE VIEW balance AS SELECT credits.account, credits - debits as balance FROM credits JOIN debits ON credits.account = debits.account AND date_diff_within(1s,credits.ts, debits.ts);
 
 -- make sure total_proton topic is created before creatint this external stream
 CREATE EXTERNAL STREAM total_s(total int) SETTINGS type='kafka', brokers='broker:29092', topic='total_proton', data_format='JSONEachRow';
